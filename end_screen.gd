75b159d7d78d83980a35aff331e48213d83406c6
Rovesib <1468172@sd61learn.ca>
2026-01-28 13:22:04 -0800

    quit buttons  and graphics

--- end_screen.gd
+++ end_screen.gd
@@ -11,3 +11,7 @@ func _ready() -> void:
 
 func _on_button_button_down() -> void:
 	get_tree().change_scene_to_file("res://rhythm_game.tscn")
+
+
+func _on_button_2_button_down() -> void:
+	get_tree().quit()
